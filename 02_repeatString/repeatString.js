f08ce7f988f082eee5978172af7cd201d58f0bed
damasake3 <171648817+damasake3@users.noreply.github.com>
2025-04-04 11:53:20 +0800

    Done exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+let string = 'hey';
+let num = 3;
 
+const repeatString = function(string, num) {
+    hey = "";
+
+    if (num > -1) {
+        for (let i = 0; i < num; i++) {
+            hey += string;
+        }
+        return hey;
+    } else {
+        return 'ERROR';
+    }
+    
 };
-
 // Do not edit below this line
 module.exports = repeatString;
