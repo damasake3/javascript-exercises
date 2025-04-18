b1e7d4c2190e7eae3246010c97279f9668c0d714
damasake3 <171648817+damasake3@users.noreply.github.com>
2025-04-18 20:43:53 +0800

    Done exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let word = input;
+    let theword = "";
+    
+    for (let i = word.length-1; i > -1; i--) {
+        theword += word[i];
+    }
+    return theword;
 };
 
 // Do not edit below this line
